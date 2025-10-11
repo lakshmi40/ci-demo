266bb1ebdc4eda270f83a3ea3b4125925c69c29e
lakshmi40 <gblakshmi61@gmail.com>
2025-10-12 00:24:51 +0530

    initial CI/CD demo commit

---
+++ ci-demo-project/src/test/java/com/jenkins/demo/ci_demo_project/AppTest.java
@@ -0,0 +1,18 @@
+package com.jenkins.demo.ci_demo_project;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Simple Test
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
